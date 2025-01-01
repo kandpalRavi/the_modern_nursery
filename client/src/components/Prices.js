e88283a2d51460b56a5362d15e2daf86a1816b32
kanpalRavi <kanpalravindra21@gmail.com>
2025-01-01 20:29:26 +0530

    front end of modern nursery

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "₹0 to 49",
+      array: [0, 49],
+    },
+    {
+      _id: 1,
+      name: "₹50 to 149",
+      array: [50, 149],
+    },
+    {
+      _id: 2,
+      name: "₹150 to 299",
+      array: [150, 299],
+    },
+    {
+      _id: 3,
+      name: "₹300 to 499",
+      array: [300, 499],
+    },
+    {
+      _id: 4,
+      name: "₹500 to 724",
+      array: [500, 724],
+    },
+    {
+      _id: 4,
+      name: "₹725 or more",
+      array: [725, 9999],
+    },
+  ];
